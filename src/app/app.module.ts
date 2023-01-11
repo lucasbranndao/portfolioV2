3771876c1a73aa3052fb0deec61d6d2edb1813c0
Lucas Brandão <lbrandaodev@gmail.com>
2023-01-11 15:37:27 -0300

    app card/rotas corrigidas / ajuste no css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,27 @@ import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent} from './component/home/home.component'
 import { SobreComponent } from './component/sobre/sobre.component';
 import { ExperienciaComponent } from './component/experiencia/experiencia.component';
-import { HomeComponent } from './component/home/home.component';
+import { CardComponent } from './component/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SobreComponent,
-    ExperienciaComponent
+    ExperienciaComponent,
+    CardComponent
 
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      {path: 'home', component: HomeComponent},
+      {path: '', component: HomeComponent},
       {path: 'sobre', component: SobreComponent},
       {path: 'experiencia', component: ExperienciaComponent},
-      {path: '', redirectTo: '/app', pathMatch: 'full'},
+
     ]),
   ],
   providers: [],
